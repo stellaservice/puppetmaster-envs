c02f826c7f8966ff025271e65ab1c8a01d0b7732
Reuben Avery <ravery2@its.jnj.com>
2015-10-31 13:51:16 -0400

    dev

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,22 +6,26 @@ node default {
 
 node 'ip-10-108-0-134.bitswarm.internal' inherits default {
   host { $fqdn:
-    ip => $ipaddress,
+    ip           => $ipaddress,
     host_aliases => ['ip-10-108-0-134', 'ip-10-108-0-134.bitswarm.internal', 'reubenavery-www', 'reubenavery-www.bitswarm.internal']
   }
 
   file { "/etc/hostname":
-    ensure => present,
-    owner => root,
-    group => root,
-    mode => 644,
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => 644,
     content => "reubenavery-www",
-    notify => Exec["set-hostname"],
   }
-
+  ~>
   exec { "set-hostname":
-    command => "/bin/hostname -F /etc/hostname",
-    unless => "/usr/bin/test `hostname` = `/bin/cat /etc/hostname`",
-    #    notify => Service[$rsyslog::params::service_name],
+    command     => "/bin/hostname -F /etc/hostname",
+    unless      => "/usr/bin/test `hostname` = `/bin/cat /etc/hostname`",
+    refreshonly => true,
+  }
+  ~>
+  exec { "reset-puppet-certs":
+    command     => "/usr/bin/find /var/lib/puppet/ssl -name '*.bitswarm.internal.pem' -delete",
+    refreshonly => true,
   }
 }
