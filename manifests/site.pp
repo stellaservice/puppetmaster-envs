717843926461527853f405f9e00bfd4c7cb0879f
Reuben Avery <ravery2@its.jnj.com>
2015-10-31 13:29:43 -0400

    dev

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,8 +4,24 @@ node default {
   class { '::bitswarm::users': }
 }
 
-node 'ip-10-108-0-134.bitswarm.local' {
-  host { 'ip-10-108-0-134.bitswarm.local':
+node 'ip-10-108-0-134.bitswarm.internal' {
+  host { 'ip-10-108-0-134.bitswarm.internal':
     ip => $ipaddress,
+    host_aliases => ['reubenavery-www.bitswarm.internal']
+  }
+
+  file { "/etc/hostname":
+    ensure => present,
+    owner => root,
+    group => root,
+    mode => 644,
+    content => "reubenavery-www",
+    notify => Exec["set-hostname"],
+  }
+
+  exec { "set-hostname":
+    command => "/bin/hostname -F /etc/hostname",
+    unless => "/usr/bin/test `hostname` = `/bin/cat /etc/hostname`",
+    notify => Service[$rsyslog::params::service_name],
   }
 }
