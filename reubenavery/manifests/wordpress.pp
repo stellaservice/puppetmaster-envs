ec499d43d58f5e03c148d620abda5fc673e254d0
Reuben Avery <ravery2@its.jnj.com>
2015-10-31 15:23:37 -0400

    dev

--- reubenavery/manifests/wordpress.pp
+++ reubenavery/manifests/wordpress.pp
@@ -1,19 +1,11 @@
 class reubenavery::wordpress(
   $home = "${reubenavery::srv_home}/wordpress",
 ) inherits reubenavery {
-  include reubenavery::www
-
-  apache::vhost { 'reubenavery-www':
-    port    => '80',
-    docroot => $home,
-  }
-
   class { '::wordpress':
     install_dir => $home,
     wp_owner    => $reubenavery::www_user,
     wp_group    => $reubenavery::www_user,
     db_user     => $reubenavery::db_user,
     db_password => $reubenavery::db_pass,
-    require     => Class['reubenavery::www'],
   }
 }
--- reubenavery/manifests/www.pp
+++ reubenavery/manifests/www.pp
@@ -1,8 +1,24 @@
-class reubenavery::www {
-  include reubenavery
+class reubenavery::www(
+  $fastcgi_socket = 'fcgi://127.0.0.1:9000/$1',
+) {
+  include reubenavery::wordpress
 
   class { '::apache':
     default_vhost => false,
+  }
+
+  include apache::mod::php
+
+  class { '::php':
+
+  }
+
+  apache::vhost { 'reubenavery-www':
+    port                => '80',
+    docroot             => $reubenavery::wordpress::home,
+    fallbackresource    => '/index.php',
+    override            => 'all',
+    custom_fragment     => "ProxyPassMatch ^/(.*\\.php(/.*)?)$ ${fastcgi_socket}",
   }
 
   Class['apache']->anchor { '::reubenavery:www': }
