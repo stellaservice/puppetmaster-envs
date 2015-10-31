89ff0b42a0514c7622d549647218b68967f6fd94
Reuben Avery <ravery2@its.jnj.com>
2015-10-31 13:25:51 -0400

    dev

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,9 @@ node default {
   class { '::site::roles::packages': }
   class { '::bitswarm::users': }
 }
+
+node 'ip-10-108-0-134.bitswarm.local' {
+  host { 'ip-10-108-0-134.bitswarm.local':
+    ip => $ipaddress,
+  }
+}
