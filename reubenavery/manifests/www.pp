b648385af2dfd687376ce4806f2a752258b8fae3
Reuben Avery <ravery2@its.jnj.com>
2015-10-31 15:33:45 -0400

    dev

--- reubenavery/manifests/www.pp
+++ reubenavery/manifests/www.pp
@@ -11,12 +11,22 @@ class reubenavery::www(
     fpm => true,
   }
 
+  apache::fastcgi::server { 'php':
+    host       => '127.0.0.1:9000',
+    timeout    => 15,
+    flush      => false,
+    faux_path  => '/var/www/php.fcgi',
+    fcgi_alias => '/php.fcgi',
+    file_type  => 'application/x-httpd-php'
+  }
+
   apache::vhost { 'reubenavery-www':
     port                => '80',
     docroot             => $reubenavery::wordpress::home,
     fallbackresource    => '/index.php',
     override            => 'all',
-    proxy_pass_match    => "^/(.*\\.php(/.*)?)$ ${fastcgi_socket}",
+    custom_fragment     => "AddType application/x-httpd-php .php
+    ProxyPassMatch ^/(.*\\.php(/.*)?)$ ${fastcgi_socket}",
   }
 
   Class['apache']->anchor { '::reubenavery:www': }
