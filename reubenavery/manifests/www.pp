229107baab8f34808fb8c73a5d31f6c1f3b7d0eb
Reuben Avery <ravery2@its.jnj.com>
2015-10-31 15:09:28 -0400

    dev

--- reubenavery/manifests/www.pp
+++ reubenavery/manifests/www.pp
@@ -1,9 +1,6 @@
 class reubenavery::www {
   class { '::apache':
-    manage_user  => false,
-    manage_group => false,
-    user         => $reubenavery::www_user,
-    group        => $reubenavery::www_user,
+    
   }
 
   Class['apache']->anchor { '::reubenavery:www': }
