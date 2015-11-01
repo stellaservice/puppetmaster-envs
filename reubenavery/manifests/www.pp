608431c44d8f98a8504a7fe526cd29d8dd544417
Reuben Avery <ravery2@its.jnj.com>
2015-11-01 13:35:25 -0500

    dev

--- reubenavery/manifests/www.pp
+++ reubenavery/manifests/www.pp
@@ -2,32 +2,7 @@ class reubenavery::www(
   $fastcgi_socket = 'fcgi://127.0.0.1:9000/$1',
 ) {
   include reubenavery
-  class { 'drupal_php':
-    apache_mods => [
-      'actions',
-      'alias',
-      'auth_basic',
-      'authn_file',
-      'authz_groupfile',
-      'authz_user',
-      'autoindex',
-      'deflate',
-      'dir',
-      'env',
-      'expires',
-      'fastcgi',
-      'headers',
-      'mime',
-      'mime_magic',
-      'negotiation',
-      'reqtimeout',
-      'rewrite',
-      'setenvif',
-      'status',
-      'suexec',
-      'xsendfile',
-    ],
-  }
+  include drupal_php
 
   #  include apache::mod::alias
   #  include apache::mod::actions
