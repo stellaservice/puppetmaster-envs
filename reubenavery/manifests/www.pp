0234b8ee543aedef82adbe348a5fe7bb1cdfaf97
Reuben Avery <ravery2@its.jnj.com>
2015-11-01 13:27:05 -0500

    dev

--- reubenavery/manifests/www.pp
+++ reubenavery/manifests/www.pp
@@ -2,23 +2,23 @@ class reubenavery::www(
   $fastcgi_socket = 'fcgi://127.0.0.1:9000/$1',
 ) {
   include reubenavery
-  include ::drupal_php
+  include drupal_php
 
-  include apache::mod::alias
-  include apache::mod::actions
-  include apache::mod::fastcgi
+#  include apache::mod::alias
+#  include apache::mod::actions
+#  include apache::mod::fastcgi
+#
+#  include php
+#  include php::fpm
 
-  include php
-  include php::fpm
-  # derp
-  apache::fastcgi::server { 'php':
-    host       => '127.0.0.1:9000',
-    timeout    => 15,
-    flush      => false,
-    faux_path  => '/var/www/php.fcgi',
-    fcgi_alias => '/php.fcgi',
-    file_type  => 'application/x-httpd-php'
-  }
+#  apache::fastcgi::server { 'php':
+#    host       => '127.0.0.1:9000',
+#    timeout    => 15,
+#    flush      => false,
+#    faux_path  => '/var/www/php.fcgi',
+#    fcgi_alias => '/php.fcgi',
+#    file_type  => 'application/x-httpd-php'
+#  }
 
 
 }
