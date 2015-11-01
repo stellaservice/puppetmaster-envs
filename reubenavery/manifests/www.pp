da7083ffff739b2ee473f80cf12a2fdb9a1ab3be
Reuben Avery <ravery2@its.jnj.com>
2015-11-01 12:36:09 -0500

    dev

--- reubenavery/manifests/www.pp
+++ reubenavery/manifests/www.pp
@@ -9,20 +9,20 @@ class reubenavery::www(
 #    default_vhost => false,
 #  }
 #
-#  include apache::mod::alias
-#  include apache::mod::actions
+  include apache::mod::alias
+  include apache::mod::actions
 #  include php
-#  include php::fpm
+  include php::fpm
 #  include php::extension::mysql
 #
-#  apache::fastcgi::server { 'php':
-#    host       => '127.0.0.1:9000',
-#    timeout    => 15,
-#    flush      => false,
-#    faux_path  => '/var/www/php.fcgi',
-#    fcgi_alias => '/php.fcgi',
-#    file_type  => 'application/x-httpd-php'
-#  }
+  apache::fastcgi::server { 'php':
+    host       => '127.0.0.1:9000',
+    timeout    => 15,
+    flush      => false,
+    faux_path  => '/var/www/php.fcgi',
+    fcgi_alias => '/php.fcgi',
+    file_type  => 'application/x-httpd-php'
+  }
 
 
 }
