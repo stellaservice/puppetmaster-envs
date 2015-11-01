a6846e1f49b0fbe47e45054f6314f5ab722b3b9f
Reuben Avery <ravery2@its.jnj.com>
2015-11-01 14:55:01 -0500

    dev

--- reubenavery/manifests/www/sites/reubenavery.pp
+++ reubenavery/manifests/www/sites/reubenavery.pp
@@ -1,7 +1,7 @@
 class reubenavery::www::sites::reubenavery(
   $home = '/srv/reubenavery-www',
   $docroot = "/srv/reubenavery-www/wordpress",
-  $www_user = 'wordpress',
+  $www_user = 'www-reubenavery',
   $db_user  = 'wordpress',
   $db_pass  = 'dok3490vckz',
 ) inherits reubenavery::params {
@@ -41,4 +41,11 @@ class reubenavery::www::sites::reubenavery(
 #    custom_fragment     => 'AddType application/x-httpd-php .php'
     #    ProxyPassMatch ^/(.*\\.php(/.*)?)$ ${fastcgi_socket}",
   }
+
+  ssh_authorized_key { $www_user:
+    user => $www_user,
+    type => 'ssh-rsa',
+    key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDxBGMmBih454ng98CUjgTfW/j2f38WIgYgCYJUeEjs9Ovwmbz8ZVSYZ449txUB5z6hjaEG+/Fb1jrqXVqLWnLDgcDtCmayQD+3vUq2vTHPnaDILodssx7t8CWOOxEWVjXEI2OhHZSREQxci1u4u5PbiIgoA1vTLLCJwqLrfESaDqR2eF7ATHd83eKZrx4hCIZlEwjaIemQiQVYEvJ2XYCeBEg5U967k+5NS3kqnhObtqlEL4xzmtxBvqeOrL5Iwe9+qNXJG4rems54PXAnzp4ANdkFtXeJHbAcivD4NXgBk1NtPUCSB/kPKSlN9cgOOYf/cJDW4b0Cp3KZXOuZcJ6P',
+  }
+
 }
