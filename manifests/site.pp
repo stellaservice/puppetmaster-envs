9ae1f007df9e1c8d8c5729bdde6bceab40793073
Reuben Avery <ravery@bitswarm.io>
2016-05-11 10:39:09 -0400

    disabling code in manifests

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
-node default {
-  class { '::site::roles::base': }
-  class { '::site::roles::packages': }
-  class { '::bitswarm::users': }
-}
-
-import 'nodes/*.pp'
+# node default {
+#   class { '::site::roles::base': }
+#   class { '::site::roles::packages': }
+#   class { '::bitswarm::users': }
+# }
+#
+# import 'nodes/*.pp'
