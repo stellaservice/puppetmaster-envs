39e762801f82b5c0d100d3132f9d48330d738cba
Reuben Avery <ravery@bitswarm.io>
2016-07-03 13:08:00 -0400

    bringing in changes from infrastructure

--- modules/bsl_core/manifests/debug.pp
+++ modules/bsl_core/manifests/debug.pp
@@ -1,7 +1,29 @@
 class bsl_core::debug(
-  $message = 'hello world'
+  $message = 'hello from bsl_core::debug (debug message not overridden)'
 ) {
-  notify { 'bsl_core_debug':
+  notify { 'bsl_core_debug (core)':
     message => "#### bsl_core_debug: ${message}"
+  }
+
+  if $::ec2_tag_profile {
+    notify { "ec2_tag_profile: ${::ec2_tag_profile}": }
+  }
+  else {
+    notify { "no ec2_tag_profile available": }
+  }
+
+  if $::ec2_tag_role {
+    notify { "ec2_tag_role: ${::ec2_tag_role}": }
+  }
+  else {
+    notify { "no ec2_tag_role available": }
+  }
+
+  $hello_worlds = hiera_array('hello_worlds', [])
+  if !empty($hello_worlds) {
+    $joined = join($hello_worlds, "\n  - ")
+    notify { '## bsl_core::debug hello_worlds':
+      message => "  - ${joined}",
+    }
   }
 }
