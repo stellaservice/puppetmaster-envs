32fed591978a85573049023706362fd6d5436957
Reuben Avery <ravery@bitswarm.io>
2016-07-04 14:01:23 -0400

    creating facts for iam again

--- modules/bsl_core/manifests/init.pp
+++ modules/bsl_core/manifests/init.pp
@@ -16,21 +16,31 @@ class bsl_core(
   ohmyzsh::plugins { ['root', $service_acct]: plugins => ['gitfast', 'colorize'] }
   ohmyzsh::theme { ['root', $service_acct]: }
 
+  file { '/etc/facter/facts.d/bsl_core':
+    ensure => directory
+  }
+
   if !empty($::ec2_metadata['iam']['info']) {
     $iam_info = parsejson($::ec2_metadata['iam']['info'])
     $iam_arn = $iam_info['InstanceProfileArn']
     $iam_profile_id = $iam_info['InstanceProfileId']
     $iam_profile_name = regsubst($iam_arn, '^.*\/', '')
-    notify { "## iam profile arn: $iam_arn": }
-    notify { "## iam profile id: $iam_profile_id": }
-    notify { "## iam profile name: $iam_profile_name": }
-    file { '/etc/facter/facts.d/bitswarm-ec2.yaml':
-      ensure => absent,
-      # content => template('bsl_core/bitswarm-ec2.yaml.erb')
+    # notify { "## iam profile arn: $iam_arn": }
+    # notify { "## iam profile id: $iam_profile_id": }
+    # notify { "## iam profile name: $iam_profile_name": }
+
+    file { '/etc/facter/facts.d/bsl_core/bitswarm-ec2.yaml':
+      ensure => file,
+      content => template('bsl_core/bitswarm-ec2.yaml.erb'),
+      require => File['/etc/facter/facts.d/bsl_core']
     }
   }
   else {
     notify { "## iam profile not found in metadata: ${::ec2_metadata['iam']['info']}": }
+    file { '/etc/facter/facts.d/bsl_core/bitswarm-ec2.yaml':
+      ensure => absent,
+      require => File['/etc/facter/facts.d/bsl_core']
+    }
   }
 
   hiera_include('classes')
