941faab164bb00a26ea0dd2787bf601c4731f44c
Reuben Avery <ravery@bitswarm.io>
2016-07-04 13:55:38 -0400

    debugging

--- modules/bsl_core/manifests/derp.pp
+++ modules/bsl_core/manifests/derp.pp
@@ -1,4 +1,4 @@
 class bsl_core::derp {
-  notify { "it was all going so well..": }
+  notify { "it was all going so well..": message => "############################## IT WAS ALL GOING SO WELL" }
 
 }
