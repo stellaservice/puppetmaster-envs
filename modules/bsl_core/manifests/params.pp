3caf487a4efbd3207f9ad1899911dd895dd92223
Reuben Avery <ravery@bitswarm.io>
2016-07-05 10:17:37 -0400

    credstash, ec2tagfacts

--- modules/bsl_core/manifests/params.pp
+++ modules/bsl_core/manifests/params.pp
@@ -9,8 +9,8 @@ class bsl_core::params {
   }
 
   $credstash_config_file = '/etc/puppet-credstash.yaml'
-  $credstash_region = 'us-west-1'
-  $credstash_dynamodb_table = 'credentials'
+  $credstash_region = 'us-east-1'
+  $credstash_dynamodb_table = 'credential-store'
   $credstash_encryption_contexts = {
     environment => 'prod',
     app_tier    => 'web',
