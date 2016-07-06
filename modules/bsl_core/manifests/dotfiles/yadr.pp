10aa4d768d10c05e9e1971f6303c8c46c40692e7
Reuben Avery <ravery@bitswarm.io>
2016-07-06 10:59:01 -0400

    Bugfix

--- modules/bsl_core/manifests/dotfiles/yadr.pp
+++ modules/bsl_core/manifests/dotfiles/yadr.pp
@@ -4,7 +4,7 @@ class bsl_core::dotfiles::yadr(
 ) inherits bsl_core::dotfiles::params {
   include '::dotfiles'
 
-  $incl_root = concat($users, 'root')
+  $incl_root = concat($users, ['root'])
 
   if $uninstall_others {
     ::ohmyzsh::uninstall { $incl_root: }
