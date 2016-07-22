2e955a18bae67af5c3d90ae47133536060c53151
Reuben Avery <ravery@bitswarm.io>
2016-07-22 18:25:55 -0400

    removing debug messaging

--- modules/bsl_core/manifests/foreman.pp
+++ modules/bsl_core/manifests/foreman.pp
@@ -1,8 +1,6 @@
 class bsl_core::foreman(
   $manage_infrastructure = 'false',
 ) {
-  notify { '## hello from CORE (environments/core) bsl_core::foreman': }
-
   if $::osfamily == 'Debian' {
     include apt
   }
@@ -11,13 +9,10 @@ class bsl_core::foreman(
   include '::bsl_puppet'
 
   if str2bool($manage_infrastructure) {
-    notify { "## managing infrastructure": }
-
     if defined('bsl_infrastructure') {
       include 'bsl_infrastructure'
     }
     else {
-      notify { 'bsl_infrastructure not available for bsl_core::foreman': }
       err('bsl_infrastructure not available for bsl_core::foreman')
     }
   }
--- modules/bsl_core/manifests/puppetmaster.pp
+++ modules/bsl_core/manifests/puppetmaster.pp
@@ -1,18 +1,6 @@
 class bsl_core::puppetmaster(
   $manage_infrastructure = 'false',
 ) {
-  # class { 'bsl_core::rancher::server':
-  #   port => 9090
-  # }
-  # class { 'bsl_core::puppetmaster::www':
-  #   puppetboard_http_port => 80,
-  #   rancher_http_port => 9090,
-  #   r10k_webhook_port => 8008,
-  #   force_ssl => false,
-  # }
-
-  notify { '## hello from CORE (environments/core) bsl_core::puppetmaster': }
-
   if $::osfamily == 'Debian' {
     include apt
   }
@@ -21,27 +9,11 @@ class bsl_core::puppetmaster(
   include '::bsl_puppet'
 
   if str2bool($manage_infrastructure) {
-    notify { "## managing infrastructure": }
-
     if defined('bsl_infrastructure') {
       include 'bsl_infrastructure'
     }
     else {
-      notify { 'bsl_infrastructure not available for bsl_core::puppetmaster': }
       err('bsl_infrastructure not available for bsl_core::puppetmaster')
     }
   }
-
-
-  # class { '::aws_scheduler':
-  #   aws_access_key_id     => $::bsl_puppet::config::server_aws_api_key,
-  #   aws_secret_access_key => $::bsl_puppet::config::server_aws_api_secret,
-  #   aws_region            => $::bsl_puppet::config::server_aws_default_region,
-  #   tag                   => 'schedule',
-  #   exclude               => '[]',
-  #   default               => '{"mon": {"start": 9, "stop": 18},"tue": {"start": 9, "stop": 18},"wed": {"start": 9, "stop": 18},"thu": {"start": 9, "stop": 18}, "fri": {"start": 9, "stop": 18}}',
-  #   time                  => 'local',
-  #   log                   => '/var/log/aws-scheduler_cron.log',
-  #   require               => Class['::bsl_puppet']
-  # }
 }
