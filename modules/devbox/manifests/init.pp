2928651dcf75abede5e44859e33958eda8dadf22
Reuben Avery <ravery@bitswarm.io>
2016-07-27 13:02:28 -0400

    rvm

--- modules/devbox/manifests/init.pp
+++ modules/devbox/manifests/init.pp
@@ -2,8 +2,9 @@ class devbox {
   notify { "## hello from devbox!": }
 
   include apt
-
-
+  include '::rvm'
+  rvm::system_user { 'ravery': }
+  
   class { 'hostname':
     hostname => 'devbox',
     domain => 'bitswarm.io'
