365e63a90d46010a57d6a5107aeb906900f153b3
Reuben Avery <ravery@bitswarm.io>
2016-07-27 13:06:20 -0400

    rvm devbox

--- modules/devbox/manifests/init.pp
+++ modules/devbox/manifests/init.pp
@@ -5,6 +5,17 @@ class devbox {
   include '::rvm'
   rvm::system_user { 'ravery': create => false }
 
+  rvm_system_ruby {
+    'ruby-1.9':
+      ensure      => 'present',
+      default_use => true,
+      build_opts  => ['--binary'];
+    'ruby-2.1':
+      ensure      => 'present',
+      default_use => true,
+      build_opts  => ['--binary']
+  }
+
   class { 'hostname':
     hostname => 'devbox',
     domain => 'bitswarm.io'
