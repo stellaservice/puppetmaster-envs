e40e719e04b012f575aa0162186c525f467bf7a1
Reuben Avery <ravery@reubenstella.local>
2016-11-21 21:46:31 -0500

    disaling most of bitswarm hiera

--- modules/bsl_core/manifests/init.pp
+++ modules/bsl_core/manifests/init.pp
@@ -6,29 +6,29 @@ class bsl_core(
     $ec2_instance_id = $::ec2_metadata['instance-id']
   }
 
-  $puppetmaster_fqdn = hiera('puppetmaster', 'puppet')
-
-  if $::ec2_metadata and $::ec2_metadata['iam'] and $::ec2_metadata['iam']['info'] {
-    $iam_info = parsejson($::ec2_metadata['iam']['info'])
-    $iam_arn = $iam_info['InstanceProfileArn']
-    $iam_profile_id = $iam_info['InstanceProfileId']
-    $iam_profile_name = regsubst($iam_arn, '^.*\/', '')
-  }
-
-  file { ['/etc/facter', '/etc/facter/facts.d']:
-    ensure => directory,
-  }
-
-  file { '/etc/facter/facts.d/bitswarm-ec2.yaml':
-    ensure  => file,
-    content => template('bsl_core/bitswarm-ec2.yaml.erb'),
-    require => File['/etc/facter/facts.d'],
-  }
-
-  if $::ec2_tag_hostname and str2bool($manage_hostname) {
-    class { 'hostname':
-      hostname => $::ec2_tag_hostname,
-      domain   => hiera('domain', $::domain),
-    }
-  }
+  # $puppetmaster_fqdn = hiera('puppetmaster', 'puppet')
+  #
+  # if $::ec2_metadata and $::ec2_metadata['iam'] and $::ec2_metadata['iam']['info'] {
+  #   $iam_info = parsejson($::ec2_metadata['iam']['info'])
+  #   $iam_arn = $iam_info['InstanceProfileArn']
+  #   $iam_profile_id = $iam_info['InstanceProfileId']
+  #   $iam_profile_name = regsubst($iam_arn, '^.*\/', '')
+  # }
+  #
+  # file { ['/etc/facter', '/etc/facter/facts.d']:
+  #   ensure => directory,
+  # }
+  #
+  # file { '/etc/facter/facts.d/bitswarm-ec2.yaml':
+  #   ensure  => file,
+  #   content => template('bsl_core/bitswarm-ec2.yaml.erb'),
+  #   require => File['/etc/facter/facts.d'],
+  # }
+  #
+  # if $::ec2_tag_hostname and str2bool($manage_hostname) {
+  #   class { 'hostname':
+  #     hostname => $::ec2_tag_hostname,
+  #     domain   => hiera('domain', $::domain),
+  #   }
+  # }
 }
